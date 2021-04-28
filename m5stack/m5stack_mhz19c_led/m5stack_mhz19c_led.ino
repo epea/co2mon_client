5b5e7a5b4fd0b3be01aa1bf3620017d1d9078778
Yoshitake Kitamura <kitamura@epea.co.jp>
2021-04-28 16:11:44 +0900

    Serial出力追加

--- m5stack/m5stack_mhz19c_led/m5stack_mhz19c_led.ino
+++ m5stack/m5stack_mhz19c_led/m5stack_mhz19c_led.ino
@@ -1,14 +1,12 @@
 #include <M5Stack.h>
 #include <MHZ19_uart.h>
+#include <ArduinoJson.h>
 
 #define RX 16
 #define TX 17
-#define INTERVAL 2
+#define INTERVAL 6
 
 #define LED_PIN 22
-
-int ledState = LOW;
-
 
 MHZ19_uart mhz19;
 
@@ -18,13 +16,16 @@ void setup(){
   M5.Lcd.print("start");
 
   Serial.begin(9600);
+  /*
+   * ライブラリデフォルトのHardwareSerialNoは2
+   * M5Stackの何かの機能で重なったら変更必要かもしれない
+   */
   //mhz19.setHardwareSerialNo(1);
   mhz19.begin(RX, TX);
   mhz19.setAutoCalibration(false);
   
   Serial.println("start");
 
-  
   delay(2000);
   pinMode(LED_PIN, OUTPUT);
 }
@@ -35,32 +36,30 @@ void loop()
 {
   if ((count % INTERVAL) == 0 ) {
     co2 = mhz19.getCO2PPM();
-    //co2=count;
     displayCo2(co2);
-    Serial.println("LED_ALERT HIGH");
-    Serial.println(co2);
-    ledState = HIGH;
-    Serial.println(ledState);
-    digitalWrite(LED_PIN,ledState);
+    writeJson(co2);
+    digitalWrite(LED_PIN,HIGH);
     m5.update();
   } else {
      M5.Lcd.print(".");
-     Serial.println("LED_ALERT LOW");
-     ledState = LOW;
-     Serial.println(ledState);
-     digitalWrite(LED_PIN,ledState);
+     digitalWrite(LED_PIN,LOW);
      m5.update();
   }
   count++;
   delay(1000);
-  
-
 }
 
 void displayCo2(int co2) {
-  Serial.println("displayCo2");
   M5.Lcd.clear(); 
   M5.Lcd.setCursor(0, 0);
   M5.Lcd.println("CO2 ppm");
   M5.Lcd.print(co2);
+}
+
+void writeJson(int co2){
+  const int capacity = JSON_OBJECT_SIZE(1);
+  StaticJsonDocument<capacity> json;
+  json["CO2"] = co2;
+  serializeJson(json, Serial);
+  Serial.println();
 }
