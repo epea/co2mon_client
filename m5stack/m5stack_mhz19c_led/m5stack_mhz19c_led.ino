6da8f095afae3671f5e425877620c6372dc0e643
Yoshitake Kitamura <kitamura@epea.co.jp>
2021-05-14 23:03:49 +0900

    wifi接続処理まで

--- m5stack/m5stack_mhz19c_led/m5stack_mhz19c_led.ino
+++ m5stack/m5stack_mhz19c_led/m5stack_mhz19c_led.ino
@@ -1,4 +1,5 @@
 #include <M5Stack.h>
+#include <WiFi.h>
 #include <BluetoothSerial.h>
 
 #include <MHZ19_uart.h>
@@ -13,58 +14,143 @@
 MHZ19_uart mhz19;
 
 /*
- * 濃度警告の閾値と切り替えフラグ
- * 以上じゃなくて「大なり」判定
- */
+   濃度警告の閾値と切り替えフラグ
+   以上じゃなくて「大なり」判定
+*/
 #define CO2_WARNING_PPM 1000
 bool _warnFlg = false;
 
-// BLE送信
+// wifi起動フラグ
+bool _wifiFlg = false;
+
+// BLE送信フラグ
 bool _bleFlg = false;
 BluetoothSerial SerialBT;
 
 void setup()
 {
   M5.begin();
+
   M5.Lcd.setBrightness(200);
   displayLCD("Start");
 
   Serial.begin(9600);
-  /*
-   * ライブラリデフォルトのHardwareSerialNoは2
-   * M5Stackの何かの機能で重なったら変更必要かもしれない
-   */
-  //mhz19.setHardwareSerialNo(1);
-  mhz19.begin(RX, TX);
-  /* 
-   * 自動キャリブレーションは切っている 
-   * 毎日外気に当てていても電圧安定していない時があるとと結構ズレる。
-   * OFFにしたら感覚的に精度±100ppm ONにしたら感覚的に精度±200ppmぐらい
-   */
-  mhz19.setAutoCalibration(false);
+  Serial.println("Start");
 
-  Serial.println("start");
+  /*
+     この中で呼んでいるESP::restart()はI2Cの再接続がうまくいかない。
+     mhzBegin()より前で呼んでいる。
+  */
+  doSettingProcess();
 
   delay(2000);
+  // LED用スレッド起動
   pinMode(LED_PIN, OUTPUT);
-
   xTaskCreatePinnedToCore(ledControlTask, "ledControlTask", 1024, NULL, 0, NULL, 0);
 
+  // MHZ起動
+  mhzBegin();
+}
+
+void doSettingProcess() {
+  Serial.println("doSettingProcess");
   M5.update();
-  
+  if (M5.BtnA.isPressed())
+  {
+    startWifi();
+    delay(1000);
+  }
   if (M5.BtnB.isPressed())
   {
-    doBLEStart();
+    startBLE();
     delay(1000);
   }
 }
 
 
-void doBLEStart(){
-  displayLCD("BLE Start");
-    _bleFlg = true;
-    SerialBT.begin("CO2Sensor");
+void startWifi() {
+  Serial.println("Wifi Start");
+  displayLCD("Wifi Start");
+  _wifiFlg = true;
+
+  connectWifi(10000, false);
+
+  Serial.println("");
+
+  if ( WiFi.status() != WL_CONNECTED ) {
+    WiFi.mode(WIFI_STA);
+    WiFi.beginSmartConfig();
+    Serial.println("Waiting for SmartConfig");
+    while (!WiFi.smartConfigDone()) {
+      delay(500);
+      Serial.print(".");
+      if ( 30000 < millis() ) {
+        Serial.println("");
+        Serial.println("Reset");
+        ESP.restart();
+      }
+    }
+
+    connectWifi(60000, true);
+
+  }
 }
+
+
+
+void connectWifi(long waitTime, bool shouldConnect) {
+  WiFi.begin();
+
+  long processStartTime = millis();
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+
+    if ( waitTime < ( millis() - processStartTime) ) {
+      Serial.println("");
+      if (shouldConnect) {
+        Serial.println("Reset");
+        ESP.restart();
+      } else {
+        Serial.println("break");
+        break;
+      }
+    }
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("");
+    Serial.println("WiFi Connected.");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+  }
+}
+
+void startBLE() {
+  Serial.println("BLE Start");
+  displayLCD("BLE Start");
+  _bleFlg = true;
+  SerialBT.begin("CO2Sensor");
+}
+
+
+void mhzBegin() {
+  Serial.println("mhzBegin");
+  /*
+     ライブラリデフォルトのHardwareSerialNoは2
+     M5Stackの何かの機能で重なったら変更必要かもしれない
+  */
+  //mhz19.setHardwareSerialNo(1);
+  mhz19.begin(RX, TX);
+  /*
+     自動キャリブレーションは切っている
+     毎日外気に当てていても電圧安定していない時があるとと結構ズレる。
+     OFFにしたら感覚的に精度±100ppm ONにしたら感覚的に精度±200ppmぐらい
+  */
+  mhz19.setAutoCalibration(false);
+}
+
 
 int count = 0;
 int co2 = 0;
@@ -75,7 +161,7 @@ void loop()
   // A,C両ボタン長押しでキャリブレーション
   if (M5.BtnA.pressedFor(1000) && M5.BtnB.releasedFor(1000) && M5.BtnC.pressedFor(1000))
   {
-    if(inCaribrationProcess){
+    if (inCaribrationProcess) {
       delay(5000);
     } else {
       inCaribrationProcess = true;
@@ -99,7 +185,7 @@ void doNormalProcess()
     writeJson(co2);
     _warnFlg = (co2 > CO2_WARNING_PPM);
 
-    if(_bleFlg){
+    if (_bleFlg) {
       SerialBT.println(co2);
     }
   }
@@ -111,7 +197,7 @@ void doNormalProcess()
   delay(1000);
 }
 
-void displayLCD(String message){
+void displayLCD(String message) {
   M5.Lcd.clear();
   M5.Lcd.fillScreen(WHITE);
   M5.Lcd.setCursor(10, 10);
